fb0b50e97da7f2d25043d7b9b5f3895216baf146
Nishikant-V <vnishikant1@gmail.com>
2026-06-16 01:53:30 +0530

    prims changes

--- Prims-Algorithm/prims.c
+++ Prims-Algorithm/prims.c
@@ -1,43 +1,64 @@
 #include <stdio.h>
 #include <limits.h>
+
 #define V 5
 
-int main() {
+int main()
+{
     int g[V][V] = {
-        {0,2,0,6,0},
-        {2,0,3,8,5},
-        {0,3,0,0,7},
-        {6,8,0,0,9},
-        {0,5,7,9,0}
-    };
+        {0, 2, 0, 6, 0},
+        {2, 0, 3, 8, 5},
+        {0, 3, 0, 0, 7},
+        {6, 8, 0, 0, 9},
+        {0, 5, 7, 9, 0}};
 
     int parent[V], key[V], mst[V];
 
     for (int i = 0; i < V; i++)
-        key[i] = INT_MAX, mst[i] = 0;
+    {
+        key[i] = INT_MAX;
+        mst[i] = 0;
+    }
 
     key[0] = 0;
     parent[0] = -1;
 
-    for (int i = 0; i < V - 1; i++) {
+    for (int i = 0; i < V - 1; i++)
+    {
         int min = INT_MAX, u;
 
         for (int j = 0; j < V; j++)
+        {
             if (!mst[j] && key[j] < min)
-                min = key[j], u = j;
+            {
+                min = key[j];
+                u = j;
+            }
+        }
 
         mst[u] = 1;
 
         for (int v = 0; v < V; v++)
+        {
             if (g[u][v] && !mst[v] && g[u][v] < key[v])
-                parent[v] = u, key[v] = g[u][v];
+            {
+                parent[v] = u;
+                key[v] = g[u][v];
+            }
+        }
     }
 
-    printf("Edge \tWeight\n");
+    int cost = 0;
+
+    printf("Edge\tWeight\n");
+
     for (int i = 1; i < V; i++)
-        printf("%d - %d \t%d\n", parent[i], i, g[i][parent[i]]);
+    {
+        printf("%d - %d\t%d\n", parent[i], i, g[i][parent[i]]);
+        cost += g[i][parent[i]];
+    }
+
+    printf("Minimum Cost = %d\n", cost);
 
     return 0;
 }
-
-
